2bd31f17a16bd6b93bcc9735d9c09ac489035b57
Zack <62723491+mziyag5187@users.noreply.github.com>
2023-03-31 23:39:44 +0300

    Create Conflict.java

---
+++ src/test/java/Conflict.java
@@ -0,0 +1,14 @@
+public class Conflict {
+    public static void main(String[] args) {
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+      System.out.println("Conflict from remote");
+
+    }
+}
