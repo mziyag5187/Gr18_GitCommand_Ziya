4242828158afb33e4a2c9b278f00684c520a9b3f
zack <mziyag5187@gmail.com>
2023-03-31 22:51:59 +0300

    my second commit

--- src/test/java/MyClass_Ziya.java
+++ src/test/java/MyClass_Ziya.java
@@ -1,5 +1,9 @@
 public class MyClass_Ziya {
     public static void main(String[] args) {
         System.out.println("First commit on my local");
+
+        System.out.println("Second commit on my local");
+
+
     }
 }
