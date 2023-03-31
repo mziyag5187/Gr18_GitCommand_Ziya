d98750db6e82061646fe50753a3050a8ca7aa78b
zack <mziyag5187@gmail.com>
2023-03-31 23:41:29 +0300

    commit for conflict

---
+++ src/test/java/Conflict.java
@@ -0,0 +1,17 @@
+public class Conflict {
+    public static void main(String[] args) {
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+        System.out.println("Conflict from my branch");
+
+
+
+    }
+}
